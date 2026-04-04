a195acf11df12c25b8adb836d7db4324d13e07bb
Marc <92420604+marcd35@users.noreply.github.com>
2026-04-04 10:13:40 -0400

    feat: Sort feeds by unread count (#96)

--- src/types/types.ts
+++ src/types/types.ts
@@ -318,12 +318,12 @@ export interface RssDashboardSettings {
     ascending: boolean;
   };
   feedSortOrder?: {
-    by: "name" | "created" | "itemCount" | "custom";
+    by: "name" | "created" | "itemCount" | "unreadCount" | "custom";
     ascending: boolean;
   };
   folderFeedSortOrders?: {
     [folderPath: string]: {
-      by: "name" | "created" | "itemCount" | "custom";
+      by: "name" | "created" | "itemCount" | "unreadCount" | "custom";
       ascending: boolean;
     };
   };
